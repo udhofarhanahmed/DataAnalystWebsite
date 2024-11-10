98ca3d83f503a181880889e190818a1b028efa46
Farhan Ahmed <udhofarhanahmed@gmail.com>
2024-11-10 08:08:47 +0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// Smooth Scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
+
+// Display a greeting based on the time of day
+function displayGreeting() {
+    const now = new Date();
+    const hours = now.getHours();
+    let greeting;
+
+    if (hours < 12) {
+        greeting = "Good Morning!";
+    } else if (hours < 18) {
+        greeting = "Good Afternoon!";
+    } else {
+        greeting = "Good Evening!";
+    }
+
+    document.getElementById('greeting').innerText = greeting;
+}
+
+// Call the function on page load
+window.onload = displayGreeting;
